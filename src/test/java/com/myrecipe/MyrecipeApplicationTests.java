e6ec6aba1393577d6236cae400748bf5f905d0aa
syr9707 <yrshin9771@gmail.com>
2023-01-12 22:36:36 +0900

    test first commit

---
+++ src/test/java/com/myrecipe/MyrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
