4af3804748e3542da2a0aa9ccf9e710de5b85a2b
syr9707 <yrshin9771@gmail.com>
2023-01-14 01:20:32 +0900

    feat(Main) : 인덱스 화면 구현

--- src/main/java/com/myrecipe/controller/MainController.java
+++ src/main/java/com/myrecipe/controller/MainController.java
@@ -1,15 +1,34 @@
 package com.myrecipe.controller;
 
+import com.myrecipe.dto.MainItemDto;
+import com.myrecipe.dto.RecipeSearchDto;
+import com.myrecipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String index() {
+    private final RecipeService recipeService;
+
+    @GetMapping(value = "/")
+    public String main(RecipeSearchDto recipeSearchDto, Optional<Integer> page, Model model){
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
+        Page<MainItemDto> recipes = recipeService.getMainItemPage(recipeSearchDto, pageable);
+
+        model.addAttribute("recipes", recipes);
+        model.addAttribute("recipeSearchDto", recipeSearchDto);
+        model.addAttribute("maxPage", 5);
+
         return "main";
     }
 
--- src/main/java/com/myrecipe/dto/MainItemDto.java
+++ src/main/java/com/myrecipe/dto/MainItemDto.java
@@ -1,2 +1,25 @@
-package com.myrecipe.dto;public class MainItemDto {
+package com.myrecipe.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MainItemDto {
+
+    private Long id;
+    private String recipeName;
+    private String recipeDetail;
+    private String imgUrl;
+    private Integer price;
+
+    @QueryProjection
+    public MainItemDto(Long id, String recipeName, String recipeDetail, String imgUrl, Integer price) {
+        this.id = id;
+        this.recipeName = recipeName;
+        this.recipeDetail = recipeDetail;
+        this.imgUrl = imgUrl;
+        this.price = price;
+    }
 }
