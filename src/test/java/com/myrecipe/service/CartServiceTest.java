d52e1cf6f8d4af9469e3b1002673110f6a6fdba6
syr9707 <yrshin9771@gmail.com>
2023-02-19 02:03:50 +0900

    test(Cart Exception) : 장바구니 예외처리 변경 테스트
    - 게시물 중복 저장

--- src/test/java/com/myrecipe/service/CartServiceTest.java
+++ src/test/java/com/myrecipe/service/CartServiceTest.java
@@ -2,14 +2,18 @@ package com.myrecipe.service;
 
 import com.myrecipe.constant.RecipeLevel;
 import com.myrecipe.dto.CartRecipeDto;
-import com.myrecipe.entity.Cart;
 import com.myrecipe.entity.CartRecipe;
 import com.myrecipe.entity.Member;
 import com.myrecipe.entity.Recipe;
 import com.myrecipe.exception.AppException;
+import com.myrecipe.exception.cart.CartException;
+import com.myrecipe.exception.cart.CartExceptionType;
+import com.myrecipe.exception.member.MemberException;
+import com.myrecipe.exception.member.MemberExceptionType;
 import com.myrecipe.repository.CartRecipeRepository;
 import com.myrecipe.repository.MemberRepository;
 import com.myrecipe.repository.RecipeRepository;
+import com.myrecipe.service.cart.CartService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -69,6 +74,24 @@ class CartServiceTest {
         assertEquals(recipe.getId(), cartRecipe.getRecipe().getId());
     }
 
+//    @Test
+//    @DisplayName("마이페이지 중복 레시피 저장 테스트")
+//    public void saveDuplicateCartTest() {
+//        Recipe recipe = saveRecipe();
+//        Member member = saveMember();
+//
+//        CartRecipeDto cartRecipeDto = new CartRecipeDto();
+//        cartRecipeDto.setRecipeId(recipe.getId());
+//
+//        Long cartRecipeId = cartService.addCart(cartRecipeDto, member.getEmail());
+//        CartRecipe cartRecipe = cartRecipeRepository.findById(cartRecipeId)
+//                .orElseThrow(EntityNotFoundException::new);
+//
+//        Throwable e = assertThrows(AppException.class, () -> {
+//            cartService.addCart(cartRecipeDto, member.getEmail());});
+//        assertEquals(recipe.getRecipeName() + "은 이미 저장되어있습니다.", e.getMessage());
+//    }
+
     @Test
     @DisplayName("마이페이지 중복 레시피 저장 테스트")
     public void saveDuplicateCartTest() {
@@ -82,9 +105,9 @@ class CartServiceTest {
         CartRecipe cartRecipe = cartRecipeRepository.findById(cartRecipeId)
                 .orElseThrow(EntityNotFoundException::new);
 
-        Throwable e = assertThrows(AppException.class, () -> {
-            cartService.addCart(cartRecipeDto, member.getEmail());});
-        assertEquals(recipe.getRecipeName() + "은 이미 저장되어있습니다.", e.getMessage());
+        assertThat(assertThrows(CartException.class,
+                () -> cartService.addCart(cartRecipeDto, member.getEmail())).getExceptionType()).isEqualTo(CartExceptionType.ALREADY_EXIST_CART);
+
     }
 
 }
