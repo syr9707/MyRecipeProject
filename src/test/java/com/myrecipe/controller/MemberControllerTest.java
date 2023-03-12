b0be72193272453127c7add3b16c639175ef2b40
syr9707 <yrshin9771@gmail.com>
2023-03-12 15:24:03 +0900

    error(MemberControllerTest): 회원정보 수정 controller 테스트 에러
    - 원인 분석 중

--- src/test/java/com/myrecipe/controller/MemberControllerTest.java
+++ src/test/java/com/myrecipe/controller/MemberControllerTest.java
@@ -1,20 +1,32 @@
 package com.myrecipe.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myrecipe.dto.MemberFormDto;
 import com.myrecipe.entity.Member;
+import com.myrecipe.repository.MemberRepository;
 import com.myrecipe.service.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc // MockMvc 테스트를 위해 선언
@@ -24,6 +36,12 @@ class MemberControllerTest {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
 
     /**
@@ -69,6 +87,34 @@ class MemberControllerTest {
                         .user(email).password("12345"))
                 .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
 
+    }
+
+    @Test
+    @DisplayName("회원정보 수정 성공 - 이름 변경")
+    @WithMockUser(roles = "ADMIN")
+    public void nameUpdateTest() throws Exception {
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+
+        String updateName = "ummPizza";
+
+        Member member = memberRepository.findByEmail(email);
+        String memberId = String.valueOf(member.getId());
+
+        Map<String, String> input = new HashMap<>();
+        input.put("password", "123456789");
+        input.put("nickname", updateName);
+
+        mockMvc.perform(put("/members/update/" + memberId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(input)))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        // then
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.get(0).getName()).isEqualTo(updateName);
     }
 
 }
